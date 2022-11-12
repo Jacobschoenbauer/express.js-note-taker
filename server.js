28a0041397ddda08b7879c71f594a77a1da80506
Jacobschoenbauer <jhwt65@gmail.com>
2022-11-12 16:08:50 -0600

    changes links they were messed at begining

--- server.js
+++ server.js
@@ -1,17 +1,24 @@
 const express = require("express");
 const path = require("path");
-
+const fs = require("fs");
 const app = express();
-const PORT = 6500;
+const PORT = process.env.PORT || 6500;
 
 app.use(express.static("public"));
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
 app.get("*", (req, res) =>
-  res.sendFile(path.join(__dirname, "public/index.html"))
+  res.sendFile(path.join(__dirname, "./public/index.html"))
 );
 app.get("/notes", (req, res) =>
-  res.sendFile(path.join(__dirname, "public/notes.html"))
+  res.sendFile(path.join(__dirname, "./public/notes.html"))
 );
+
+
+
 
 app.listen(PORT, () => {
   console.log(`The Port being used ${PORT}!`);
