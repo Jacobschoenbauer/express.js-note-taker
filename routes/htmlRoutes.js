88b39b41697f72216cdcc14ebd94a2492f432b0c
Jacobschoenbauer <jhwt65@gmail.com>
2022-11-13 10:43:11 -0600

    need to json html

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,11 +1,12 @@
 const path = require("path");
-const express = require("express").Router();
+const router = require("express").Router();
 
-app.get("/notes", (req, res) =>
+router.get("/notes", (req, res) =>
   res.sendFile(path.join(__dirname, "../public/notes.html"))
 );
 
-app.get("*", (req, res) =>
+router.get("*", (req, res) =>
   res.sendFile(path.join(__dirname, "../public/index.html"))
 );
 
+module.exports = router;
