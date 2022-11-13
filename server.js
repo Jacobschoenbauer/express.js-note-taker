aa6410bc05153619a4f498b0521fb67cfdf9e5b5
Jacobschoenbauer <jhwt65@gmail.com>
2022-11-13 09:33:02 -0600

    trouble shooting

--- server.js
+++ server.js
@@ -11,15 +11,35 @@ app.use(express.json());
 app.use(express.static("public"));
 
 app.get("*", (req, res) =>
-  res.sendFile(path.join(__dirname, "./public/index.html"))
+  res.sendFile(path.join(__dirname, "./public/inex.html"))
 );
 app.get("/notes", (req, res) =>
   res.sendFile(path.join(__dirname, "./public/notes.html"))
 );
 
+fs.readFile("./db/db.json", "utf8", (err, data) => {
+  if (err) {
+    console.error(err);
+  } else {
+    const parsedData = JSON.parse(data);
+    parsedData.push(parsedData);
+    //writeToFile(file, parsedData);
+    app.get("/api/notes", function (req, res) {
+      res.json(parsedData);
+    });
 
+    app.post("/api/notes", function (req, res) {
+      let newNote = req.body;
+      parsedData.push(newNote);
+      updateDb();
+      return console.log("Added new note: " + newNote.title);
+    });
 
-
+    app.get("/api/notes/:id", function (req, res) {
+      res.json(parsedData[req.params.id]);
+    });
+  }
+});
 
 app.listen(PORT, () => {
   console.log(`The Port being used ${PORT}!`);
