12e97d59d2ec43d980dd1709558304683d745b8e
Jacobschoenbauer <jhwt65@gmail.com>
2022-11-13 14:56:44 -0600

    broke the code

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,15 +1,18 @@
 const util = require("util")
 const fs = require("fs")
-
 const router = require("express").Router();
+const uuidv1 = require("uuid/v1")
+const readFileAsync = util.promisify(fs.readFile)
+const writeFileAsync = util.promisify(fs.writeFile)
 
-router.get("/api/notes", function (req, res) {
+
+router.get("/notes", function (req, res) {
   let parsedData= util.promisify.apply(fs.readFile("../db/db.json", "utf8"))
   console.log(parsedData)
     res.json(parsedData);
 });
 
-router.post("/api/notes", function (req, res) {
+router.post("/notes", function (req, res) {
   let newNote = req.body;
   parsedData.push(newNote);
   updateDb();
--- server.js
+++ server.js
@@ -1,24 +1,17 @@
 const express = require("express");
-const path = require("path");
-const fs = require("fs");
 const app = express();
 const PORT = process.env.PORT || 6500;
 const apiRoutes = require("./routes/apiRoutes.js")
 const htmlRoutes = require("./routes/htmlRoutes.js")
-
+app.use(express.json());
 app.use(express.static("public"));
 
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+
 app.use(express.static("public"));
 app.use("/api", apiRoutes)
 app.use("/", htmlRoutes)
-//app.get("*", (req, res) =>
-//  res.sendFile(path.join(__dirname, "./public/index.html"))
-//);
-//app.get("/notes", (req, res) =>
- // res.sendFile(path.join(__dirname, "./public/notes.html"))
-//);
+
 
 
 
