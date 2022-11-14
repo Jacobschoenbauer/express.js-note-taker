c7e0955451e61bfaa87a1ef0a8b82e57faf24d16
Jacobschoenbauer <jhwt65@gmail.com>
2022-11-14 10:32:23 -0600

    working on html problem

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,21 +1,26 @@
 const util = require("util")
 const fs = require("fs")
 const router = require("express").Router();
-const uuidv1 = require("uuid/v1")
-const readFileAsync = util.promisify(fs.readFile)
-const writeFileAsync = util.promisify(fs.writeFile)
+//const uuidv1 = require("uuid/v1")
+//const readFileAsync = util.promisify(fs.readFile)
+//const writeFileAsync = util.promisify(fs.writeFile)
 
 
 router.get("/notes", function (req, res) {
-  let parsedData= util.promisify.apply(fs.readFile("../db/db.json", "utf8"))
-  console.log(parsedData)
-    res.json(parsedData);
+  router.get("/notes", function (req, res) {
+    let parsedData= util.promisify.apply(fs.readFile("../db/db.json", "utf8"))
+    console.log(parsedData)
+      res.json(parsedData);
+  });
+  
+        console.log(parsedData)
+    res.json();
 });
 
 router.post("/notes", function (req, res) {
   let newNote = req.body;
   parsedData.push(newNote);
-  updateDb();
+  updateDb(newNote);
   return console.log("Added new note: " + newNote.title);
 });
 
--- server.js
+++ server.js
@@ -1,13 +1,11 @@
 const express = require("express");
-const app = express();
-const PORT = process.env.PORT || 6500;
+
 const apiRoutes = require("./routes/apiRoutes.js")
 const htmlRoutes = require("./routes/htmlRoutes.js")
+const app = express();
+const PORT = process.env.PORT || 6500;
 app.use(express.json());
-app.use(express.static("public"));
-
 app.use(express.urlencoded({ extended: true }));
-
 app.use(express.static("public"));
 app.use("/api", apiRoutes)
 app.use("/", htmlRoutes)
