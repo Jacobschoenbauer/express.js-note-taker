e8cb4085a0a3de7d90b0cd497f27849dab0b10a6
Jacobschoenbauer <jhwt65@gmail.com>
2022-11-14 10:41:56 -0600

    route in a route

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,20 +1,14 @@
-const util = require("util")
-const fs = require("fs")
+const util = require("util");
+const fs = require("fs");
 const router = require("express").Router();
 //const uuidv1 = require("uuid/v1")
 //const readFileAsync = util.promisify(fs.readFile)
 //const writeFileAsync = util.promisify(fs.writeFile)
 
-
 router.get("/notes", function (req, res) {
-  router.get("/notes", function (req, res) {
-    let parsedData= util.promisify.apply(fs.readFile("../db/db.json", "utf8"))
-    console.log(parsedData)
-      res.json(parsedData);
-  });
-  
-        console.log(parsedData)
-    res.json();
+  let parsedData = util.promisify.apply(fs.readFile("../db/db.json", "utf8"));
+  console.log(parsedData);
+  res.json(parsedData);
 });
 
 router.post("/notes", function (req, res) {
