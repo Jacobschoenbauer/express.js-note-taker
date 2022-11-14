d029e390eb1c628b7192c84a441761eb5427b02f
Jacobschoenbauer <jhwt65@gmail.com>
2022-11-14 11:42:15 -0600

    changed routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -4,9 +4,13 @@ const router = require("express").Router();
 //const uuidv1 = require("uuid/v1")
 //const readFileAsync = util.promisify(fs.readFile)
 //const writeFileAsync = util.promisify(fs.writeFile)
-const storage = require("../db/db.json");
+//const storage = require("../db/db.json");
 router.get("/notes", function (req, res) {
-  res.json(storage);
+  console.log(process.cwd());
+  fs.readFile("./db/db.json", "utf-8", (err, data) => {
+    if (err) throw err;
+    res.json(data);
+  });
 });
 
 router.post("/notes", function (req, res) {
