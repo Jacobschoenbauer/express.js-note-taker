854b45a9f0293f740d61a256329b9cb1fea0dfbf
Jacobschoenbauer <jhwt65@gmail.com>
2022-11-15 11:41:14 -0600

    adjusted apiRoutes.js it works

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,26 +1,39 @@
 const util = require("util");
 const fs = require("fs");
 const router = require("express").Router();
-//const uuidv1 = require("uuid/v1")
-//const readFileAsync = util.promisify(fs.readFile)
-//const writeFileAsync = util.promisify(fs.writeFile)
-//const storage = require("../db/db.json");
+
+function readNotes() {
+  const notes = fs.readFileSync("db/db.json", "utf-8");
+  return JSON.parse(notes);
+}
+function writeNotes(notes) {
+  fs.writeFileSync("db/db.json", JSON.stringify(notes));
+}
+function generateId() {
+  return Math.random();
+}
 router.get("/notes", function (req, res) {
-  console.log(process.cwd());
-  fs.readFile("./db/db.json", "utf-8", (err, data) => {
-    if (err) throw err;
-    res.json(data);
-  });
+  const notes = readNotes();
+  res.json(notes);
+
 });
 
 router.post("/notes", function (req, res) {
+  const notes = readNotes();
   let newNote = req.body;
-  parsedData.push(newNote);
-  updateDb(newNote);
-  return console.log("Added new note: " + newNote.title);
+  newNote.id = generateId();
+  notes.push(newNote);
+  writeNotes(notes);
+  res.json(newNote);
 });
 
-router.get("/api/notes/:id", function (req, res) {
-  res.json(parsedData[req.params.id]);
+router.delete("/notes/:id", function (req, res) {
+  const notes = readNotes();
+  console.log(notes);
+
+  const updatedNotes = notes.filter((note) => note.id != req.params.id);
+  writeNotes(updatedNotes);
+  res.json({ ok: true });
 });
+
 module.exports = router;
